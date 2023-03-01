e10863633afe545fc82986601ba2ecdec9961e71
cdb61092 <36825464+cdb61092@users.noreply.github.com>
2023-02-28 20:31:23 -0800

    create update and delete note methods

--- server/routers/note.ts
+++ server/routers/note.ts
@@ -8,27 +8,59 @@ export const noteRouter = router({
             z.object({
                 title: z.string(),
                 content: z.string(),
-                tags: z.array(z.string()),
+                tags: z.array( z.string() ),
             }))
-            .mutation(async ({ input, ctx }) => {
-                return prisma.note.create({
-                    data: {
-                        title: input.title,
-                        content: input.content,
-                        tags: input.tags,
-                        user_id: ctx.user.id,
-                    }
-                })
-            }),
-            getNotes: protectedProcedure.query(async ({ctx}) => {
-                return prisma.note.findMany({
-                    where: {
-                        user_id: ctx.user.id,
-                    }
-                })
+        .mutation(async ({ input, ctx }) => {
+            return prisma.note.create({
+                data: {
+                    title: input.title,
+                    content: input.content,
+                    tags: input.tags,
+                    user_id: ctx.user.id,
+                }
             })
-            
-        });
+        }),
+    getNotes: protectedProcedure.query(async ({ ctx }) => {
+        return prisma.note.findMany({
+            where: {
+                user_id: ctx.user.id,
+            }
+        })
+    }),
+    updateNote: protectedProcedure
+        .input(
+            z.object({
+            user_id: z.number(),
+            title: z.string(),
+            content: z.string(),
+            tags: z.array(z.string()),
+        }))
+        .mutation(async ({ input }) => {
+            return prisma.note.update({
+                where: {
+                    id: input.user_id,
+                },
+                data: {
+                    title: input.title,
+                    content: input.content,
+                    tags: input.tags,
+                }
+            })
+    }),
+    deleteNote: protectedProcedure
+        .input(
+            z.object({
+                id: z.number(),
+            }))
+        .mutation(async ({ input }) => {
+            return prisma.note.delete({
+                where: {
+                    id: input.id,
+                }
+            })
+    }),
+         
+});
     
         
 
